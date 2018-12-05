ee2e10146ddf7d793e9a58fbdbade888586c1797
Pierre Chabannon <pierrec@mbp-de-pierre.lan>
2018-12-05 16:37:42 +0100

    modif syntaxe et modeles

--- parts/connexionCompany.js
+++ parts/connexionCompany.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
-// const bodyParser = require("body-parser");
-// app.use(bodyParser.json());
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
 const uid2 = require("uid2");
 var validator = require("validator");
-var User = require("../models/User");
+var Company = require("../models/Company");
 
-app.post("/api/sign_up", function(req, res) {
+app.post("/sign_up_company", function(req, res) {
 	if (validator.isEmail(req.body.email) === false) {
 		return res.status(400).json({ message: "Invalid email" });
 	}
@@ -17,38 +17,42 @@ app.post("/api/sign_up", function(req, res) {
 	const salt = uid2(16);
 	const hash = SHA256(password + salt).toString(encBase64);
 
-	const newUser = new User({
+	const newCompany = new Company({
 		email: req.body.email,
 		token: uid2(16),
 		salt: salt,
-		hash: hash
+		hash: hash,
+		companyAccount: {
+			companyName: req.body.companyName
+		}
 	});
 
-	newUser.save(function(err, userSaved) {
+	newCompany.save(function(err, companySaved) {
 		if (err) {
 			res.status(400).json("err: err.message");
 		} else {
 			res.json({
-				_id: userSaved._id,
-				token: userSaved.token,
-				email: userSaved.email,
-				account: userSaved.account
+				_id: companySaved._id,
+				token: companySaved.token,
+				email: companySaved.email,
+				companyAccount: companySaved.companyAccount
 			});
 		}
 	});
 });
 
-app.post("/api/log_in", function(req, res, next) {
-	User.findOne({ email: req.body.email }).exec(function(err, user) {
+app.post("/log_in_company", function(req, res, next) {
+	Company.findOne({ email: req.body.email }).exec(function(err, company) {
 		if (err) return next(err.message);
-		if (user) {
+		if (company) {
 			if (
-				SHA256(req.body.password + user.salt).toString(encBase64) === user.hash
+				SHA256(req.body.password + company.salt).toString(encBase64) ===
+				company.hash
 			) {
 				return res.json({
-					_id: user._id,
-					token: user.token,
-					account: user.account
+					_id: company._id,
+					token: company.token,
+					companyAccount: company.companyAccount
 				});
 			} else {
 				return res.status(401).json({ error: "Unauthorized" });
