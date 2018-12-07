5ee0c95f7632dc45997edd2e250f1276fc9a0054
Philippe MOUIEL <philippemouiel@mbp-de-philippe.lan>
2018-12-07 17:47:31 +0100

    modif

--- models/User.js
+++ models/User.js
@@ -1,37 +1,37 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-	email: String,
-	password: String,
-	token: String, // Le token permettra d'authentifier l'utilisateur
-	hash: String,
-	salt: String,
-	account: {
-		firstName: String,
-		lastName: String,
-		age: Number,
-		sex: String,
-		adress: [
-			{
-				latitude: Number,
-				longitude: Number,
-				latitudeDelta: Number,
-				longitudeDelta: Number
-			},
-			{ country: String },
-			{ city: String },
-			{ street: String },
-			{ number: Number },
-			{ postCode: Number }
-		],
-		phone: String,
-		bankAmount: Number,
-		description: String,
-		bankAccount: { bic: String, iban: String },
-		photo: String,
-		criterias: [{ type: mongoose.Schema.Types.ObjectId, ref: "Criteria" }],
-		userOffers: { type: mongoose.Schema.Types.ObjectId, ref: "Offer" }
-	}
+  email: String,
+  password: String,
+  token: String, // Le token permettra d'authentifier l'utilisateur
+  hash: String,
+  salt: String,
+  account: {
+    firstName: String,
+    lastName: String,
+    birthDate: Date,
+    sex: String,
+    adress: [
+      {
+        latitude: Number,
+        longitude: Number,
+        latitudeDelta: Number,
+        longitudeDelta: Number
+      },
+      { country: String },
+      { city: String },
+      { street: String },
+      { number: Number },
+      { postCode: Number }
+    ],
+    phone: String,
+    bankAmount: Number,
+    description: String,
+    bankAccount: { bic: String, iban: String },
+    photo: String,
+    criterias: [{ type: mongoose.Schema.Types.ObjectId, ref: "Criteria" }],
+    userOffers: { type: mongoose.Schema.Types.ObjectId, ref: "Offer" }
+  }
 });
 
 module.exports = mongoose.model("User", UserSchema, "users");
